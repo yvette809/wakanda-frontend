1ec7fbfef5a4ffa5749c265099c74d3e9f47b7fe
Yvette <nchombuayvta@gmil.com>
2020-10-10 22:37:30 +0200

    updats

--- src/components/dashboard/DashBoardActions.js
+++ src/components/dashboard/DashBoardActions.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import { Link } from "react-router-dom";
 
-const DashboardActions = () => {
-  return (
-    <div>
-      <Link to="/edit-profile" className="btn btn-light">
-        <i className="fa fa-user-circle text-primary" /> Edit Profile
-      </Link>
-      <Link to="/add-experience" className="btn btn-light">
-        <i className="fa fa-black-tie text-primary" /> Add Experience
-      </Link>
-    </div>
-  );
-};
+// const DashboardActions = () => {
+//   return (
+//     <div>
+//       <Link to="/edit-profile" className="btn btn-light">
+//         <i className="fa fa-user-circle text-primary" /> Edit Profile
+//       </Link>
+//       <Link to="/add-experience" className="btn btn-light">
+//         <i className="fa fa-black-tie text-primary" /> Add Experience
+//       </Link>
+//     </div>
+//   );
+// };
 
-export default DashboardActions;
+// export default DashboardActions;
